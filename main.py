df0b454e78a218732b2fed912f4ebed5a4d25fcf
Andrew VALLETEAU <avallete@e1r2p17.42.fr>
2017-02-23 20:27:04 +0100

    [FEAT] Parse input

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import sys
 
-def resolve(input):
-    pass
-
 def get_input(fd):
     lines = []
     for line in fd.readlines():
@@ -12,16 +9,36 @@ def get_input(fd):
 def split_line(line):
     return [c for c in line]
 
+
+def parse_endpoint(latency, caches_numbers, input):
+    endpoint = {'caches': [], 'videos': [], 'data_center_latency': latency}
+    for i in range(0, caches_numbers):
+        cache_id, cache_latency = input.pop(0).split(' ')
+        endpoint['caches'].append((int(cache_id), int(cache_latency)))
+    return endpoint
+
+def parse_endpoints_and_requests(input, endpoint_nb, request_nb):
+    endpoints = []
+
+    for i in range(0, (int(endpoint_nb))):
+        latency, caches_numbers = input.pop(0).split(' ')
+        if int(caches_numbers) > 0:
+            endpoints.append(parse_endpoint(int(latency), int(caches_numbers), input))
+        else:
+            endpoints.append({'caches': [], 'videos': [], 'data_center_latency': int(latency)})
+    for k in range(0, int(request_nb)):
+        video_id, endpoint_id, requests = input.pop(0).split(' ')
+        endpoints[int(endpoint_id)]['videos'].append((int(video_id), int(requests)))
+    return endpoints
+
 def app_run():
     fd = open(sys.argv[1], "r")
     input = get_input(fd)
-    pizza = []
-    #(R, C, L, H) = input.pop(0).split(' ')
-    for line in input:
-        print (split_line(line))
-        pizza.append(split_line(line))
-    
-    #import ipdb; ipdb.set_trace()
+
+    videos_nb, endpoint_nb, request_nb, caches_nb, caches_size = input.pop(0).split(' ')
+    videos_sizes = input.pop(0).split(' ')
+    endpoints = parse_endpoints_and_requests(input, int(endpoint_nb), int(request_nb))
+
     fd.close()
 
 if __name__ == "__main__":
@@ -29,4 +46,4 @@ if __name__ == "__main__":
         if len(sys.argv) == 2:
             app_run()
     except OSError as e:
-        print("Error")
+        print("Error %s" % e)
